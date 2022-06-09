4a8279a2a697c1b7f9bf7b20958a265044ee2935
Rahul Anand <rahulanand16nov@gmail.com>
2022-06-09 16:40:03 +0530

    bump sdk version to 0.2 (#11)

--- src/filter/root_context.rs
+++ src/filter/root_context.rs
@@ -1,6 +1,6 @@
 use crate::configuration::FilterConfig;
 use crate::filter::http_context::Filter;
-use log::{info, warn};
+use log::{info, warn, debug};
 use proxy_wasm::traits::{Context, HttpContext, RootContext};
 use proxy_wasm::types::ContextType;
 
@@ -23,7 +23,7 @@ impl RootContext for FilterRoot {
     }
 
     fn on_configure(&mut self, _config_size: usize) -> bool {
-        let configuration: Vec<u8> = match self.get_configuration() {
+        let configuration: Vec<u8> = match self.get_plugin_configuration() {
             Some(c) => c,
             None => return false,
         };
