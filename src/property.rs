26408a286ad798a41c452995b752aedbd031863e
Eguzki Astiz Lezaun <eastizle@redhat.com>
2024-10-17 00:27:05 +0200

    remote_address: step 2 simplified alternative implementation
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- src/property.rs
+++ src/property.rs
@@ -2,39 +2,26 @@ use crate::property_path::Path;
 use log::debug;
 use log::warn;
 use proxy_wasm::hostcalls;
-use proxy_wasm::types::MapType;
 use proxy_wasm::types::Status;
 
 fn remote_address() -> Result<Option<Vec<u8>>, Status> {
-    // From https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_conn_man/headers#x-forwarded-for
-    // If use_remote_address is set to true, Envoy sets the x-envoy-external-address header to the trusted client address.
-    debug!("get_map_value(MapType::HttpRequestHeaders, x-envoy-external-address");
-    match hostcalls::get_map_value(MapType::HttpRequestHeaders, "x-envoy-external-address")? {
+    // Ref https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_conn_man/headers#x-forwarded-for
+    // Envoy sets source.address to the trusted client address AND port.
+    match host_get_property("source.address")? {
         None => {
-            debug!("x-envoy-external-address property not found");
-            // When the incoming request does not have XFF header,
-            // envoy does not set x-envoy-external-address regardless of the xff_num_trusted_hops
-            // value. The request is considered originated internally.
-            // For this case, the trusted client address is the source IP address
-            // of the immediate downstream node’s connection to Envoy
-            match host_get_property("source.address")? {
-                None => {
-                    warn!("source.address property not found");
-                    Err(Status::BadArgument)
-                }
-                Some(host_vec) => match String::from_utf8(host_vec) {
-                    Err(e) => {
-                        warn!("source.address property value not string: {}", e);
-                        Err(Status::BadArgument)
-                    }
-                    Ok(source_address) => {
-                        let split_address = source_address.split(':').collect::<Vec<_>>();
-                        Ok(Some(split_address[0].as_bytes().to_vec()))
-                    }
-                },
-            }
+            warn!("source.address property not found");
+            Err(Status::BadArgument)
         }
-        Some(trusted_client_address) => Ok(Some(trusted_client_address.as_bytes().to_vec())),
+        Some(host_vec) => match String::from_utf8(host_vec) {
+            Err(e) => {
+                warn!("source.address property value not string: {}", e);
+                Err(Status::BadArgument)
+            }
+            Ok(source_address) => {
+                let split_address = source_address.split(':').collect::<Vec<_>>();
+                Ok(Some(split_address[0].as_bytes().to_vec()))
+            }
+        },
     }
 }
 
