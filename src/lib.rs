303a689154bb44434c0303f9d410ab8f135e16e2
Adam Cattermole <acatterm@redhat.com>
2025-01-15 15:23:57 +0000

    Move filter.rs into src/filter/mod and start function into lib
    Signed-off-by: Adam Cattermole <acatterm@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,43 @@ mod runtime_action_set;
 mod runtime_config;
 mod service;
 
+#[cfg_attr(
+    all(
+        target_arch = "wasm32",
+        target_vendor = "unknown",
+        target_os = "unknown"
+    ),
+    export_name = "_start"
+)]
+#[cfg_attr(
+    not(all(
+        target_arch = "wasm32",
+        target_vendor = "unknown",
+        target_os = "unknown"
+    )),
+    allow(dead_code)
+)]
+// This is a C interface, so make it explicit in the fn signature (and avoid mangling)
+extern "C" fn start() {
+    use crate::filter::root_context::FilterRoot;
+    use log::info;
+    use proxy_wasm::traits::RootContext;
+    use proxy_wasm::types::LogLevel;
+
+    proxy_wasm::set_log_level(LogLevel::Trace);
+    std::panic::set_hook(Box::new(|panic_info| {
+        proxy_wasm::hostcalls::log(LogLevel::Critical, &panic_info.to_string())
+            .expect("failed to log panic_info");
+    }));
+    proxy_wasm::set_root_context(|context_id| -> Box<dyn RootContext> {
+        info!("#{} set_root_context", context_id);
+        Box::new(FilterRoot {
+            context_id,
+            action_set_index: Default::default(),
+        })
+    });
+}
+
 #[cfg(test)]
 mod tests {
     use crate::envoy::{HeaderValue, RateLimitResponse, RateLimitResponse_Code};
