497cb1c63d9dd1db579dfbdff41fd19b828ba3eb
Adam Cattermole <acatterm@redhat.com>
2025-09-24 14:22:46 +0100

    Create the envoy/mod for prost
    Signed-off-by: Adam Cattermole <acatterm@redhat.com>

--- src/envoy/mod.rs
+++ src/envoy/mod.rs
@@ -1,49 +1,115 @@
-mod address;
-mod attribute_context;
-mod authority;
-mod backoff;
-mod base;
-mod config_source;
-mod context_params;
-mod custom_tag;
-mod extension;
-mod external_auth;
-mod grpc_service;
-mod http_status;
-mod http_uri;
-mod matcher;
-mod metadata;
-mod number;
-mod percent;
-mod proxy_protocol;
-mod range;
-mod ratelimit;
-mod ratelimit_unit;
-mod regex;
-mod rls;
-mod route_components;
-mod semantic_version;
-mod socket_option;
-mod status;
-mod string;
-mod timestamp;
-mod token_bucket;
-mod value;
+// Generated protobuf modules
+pub mod envoy {
+    pub mod config {
+        pub mod common {
+            pub mod matcher {
+                pub mod v3 {
+                    include!("envoy.config.common.matcher.v3.rs");
+                }
+            }
+        }
+        pub mod core {
+            pub mod v3 {
+                include!("envoy.config.core.v3.rs");
+            }
+        }
+        pub mod route {
+            pub mod v3 {
+                include!("envoy.config.route.v3.rs");
+            }
+        }
+    }
+    pub mod extensions {
+        pub mod common {
+            pub mod ratelimit {
+                pub mod v3 {
+                    include!("envoy.extensions.common.ratelimit.v3.rs");
+                }
+            }
+        }
+    }
+    pub mod service {
+        pub mod auth {
+            pub mod v3 {
+                include!("envoy.service.auth.v3.rs");
+            }
+        }
+        pub mod ratelimit {
+            pub mod v3 {
+                include!("envoy.service.ratelimit.v3.rs");
+            }
+        }
+    }
+    pub mod r#type {
+        pub mod matcher {
+            pub mod v3 {
+                include!("envoy.r#type.matcher.v3.rs");
+            }
+        }
+        pub mod metadata {
+            pub mod v3 {
+                include!("envoy.r#type.metadata.v3.rs");
+            }
+        }
+        pub mod tracing {
+            pub mod v3 {
+                include!("envoy.r#type.tracing.v3.rs");
+            }
+        }
+        pub mod v3 {
+            include!("envoy.r#type.v3.rs");
+        }
+    }
+}
 
-pub use {
-    address::{Address, SocketAddress},
-    attribute_context::{
-        AttributeContext, AttributeContext_HttpRequest, AttributeContext_Peer,
-        AttributeContext_Request,
-    },
-    base::{HeaderValue, HeaderValueOption, Metadata},
-    external_auth::{
-        CheckRequest, CheckResponse, CheckResponse_oneof_http_response, DeniedHttpResponse,
-    },
-    http_status::StatusCode,
-    ratelimit::{RateLimitDescriptor, RateLimitDescriptor_Entry},
-    rls::{RateLimitRequest, RateLimitResponse, RateLimitResponse_Code},
+pub mod google {
+    pub mod rpc {
+        include!("google.rpc.rs");
+    }
+}
+
+pub mod udpa {
+    pub mod annotations {
+        include!("udpa.annotations.rs");
+    }
+}
+
+pub mod xds {
+    pub mod annotations {
+        pub mod v3 {
+            include!("xds.annotations.v3.rs");
+        }
+    }
+    pub mod core {
+        pub mod v3 {
+            include!("xds.core.v3.rs");
+        }
+    }
+    pub mod r#type {
+        pub mod matcher {
+            pub mod v3 {
+                include!("xds.r#type.matcher.v3.rs");
+            }
+        }
+    }
+}
+
+pub mod validate {
+    include!("validate.rs");
+}
+
+pub use envoy::config::core::v3::{
+    address, socket_address, Address, HeaderValue, HeaderValueOption, Metadata, SocketAddress,
 };
+pub use envoy::extensions::common::ratelimit::v3::{rate_limit_descriptor, RateLimitDescriptor};
+pub use envoy::r#type::v3::StatusCode;
+pub use envoy::service::auth::v3::{
+    attribute_context, check_response, AttributeContext, CheckRequest, CheckResponse,
+    DeniedHttpResponse,
+};
+pub use envoy::service::ratelimit::v3::{rate_limit_response, RateLimitRequest, RateLimitResponse};
 
 #[cfg(test)]
-pub use {external_auth::OkHttpResponse, http_status::HttpStatus};
+pub use envoy::r#type::v3::HttpStatus;
+#[cfg(test)]
+pub use envoy::service::auth::v3::OkHttpResponse;
