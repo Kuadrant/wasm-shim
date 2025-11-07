2bae3e33d0c325f2a5e1977944ff29ddbf80c523
Adam Cattermole <acatterm@redhat.com>
2025-11-07 15:53:49 +0000

    Create the FailureModeTask
    Signed-off-by: Adam Cattermole <acatterm@redhat.com>

---
+++ src/v2/kuadrant/pipeline/tasks/failure_mode.rs
@@ -0,0 +1,43 @@
+use crate::v2::kuadrant::{
+    pipeline::tasks::{SendReplyTask, Task, TaskOutcome},
+    ReqRespCtx,
+};
+
+struct FailureModeTask {
+    task: Box<dyn Task>,
+    abort: bool,
+}
+
+impl Task for FailureModeTask {
+    fn apply(self: Box<Self>, ctx: &mut ReqRespCtx) -> TaskOutcome {
+        match self.task.apply(ctx) {
+            TaskOutcome::Failed => {
+                if self.abort {
+                    TaskOutcome::Terminate(Box::new(SendReplyTask::default()))
+                } else {
+                    TaskOutcome::Done
+                }
+            }
+            TaskOutcome::Deferred { token_id, pending } => TaskOutcome::Deferred {
+                token_id,
+                pending: Box::new(FailureModeTask {
+                    task: pending,
+                    abort: self.abort,
+                }),
+            },
+            outcome => outcome,
+        }
+    }
+
+    fn id(&self) -> Option<String> {
+        self.task.id()
+    }
+
+    fn dependencies(&self) -> &[String] {
+        self.task.dependencies()
+    }
+
+    fn pauses_filter(&self, ctx: &ReqRespCtx) -> bool {
+        self.task.pauses_filter(ctx)
+    }
+}
