86e317c523bc069a72ce382e67d42caaf9076ae0
Adam Cattermole <a.d.cattermole@gmail.com>
2025-11-18 13:13:55 +0000

    Initialise ProxyWasmOtlpExporter
    Signed-off-by: Adam Cattermole <a.d.cattermole@gmail.com>

---
+++ src/tracing/mod.rs
@@ -0,0 +1,21 @@
+mod exporter;
+
+pub use exporter::ProxyWasmOtlpExporter;
+
+use opentelemetry::trace::TracerProvider;
+use opentelemetry_sdk::trace::SdkTracerProvider;
+
+pub fn otlp_layer<S>(endpoint: impl Into<String>) -> impl tracing_subscriber::Layer<S>
+where
+    S: ::tracing::Subscriber + for<'a> tracing_subscriber::registry::LookupSpan<'a>,
+{
+    let exporter = ProxyWasmOtlpExporter::new(endpoint);
+
+    let provider = SdkTracerProvider::builder()
+        .with_simple_exporter(exporter)
+        .build();
+
+    let tracer = provider.tracer("wasm-shim");
+
+    tracing_opentelemetry::layer().with_tracer(tracer)
+}
