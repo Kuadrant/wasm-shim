eaba9f39fcb631b944e071adb437b2025b42dc42
Eguzki Astiz Lezaun <eastizle@redhat.com>
2026-09-21 10:09:02 +0200

    fix: test missing body_extraction_misses
    Signed-off-by: Eguzki Astiz Lezaun <eastizle@redhat.com>

--- crates/wasm-shim/tests/failures.rs
+++ crates/wasm-shim/tests/failures.rs
@@ -894,6 +894,11 @@ fn it_does_not_strand_a_sequential_successor_when_a_task_fails() {
         .returning(Some(5))
         .expect_define_metric(Some(MetricType::Counter), Some("kuadrant.errors"))
         .returning(Some(6))
+        .expect_define_metric(
+            Some(MetricType::Counter),
+            Some("kuadrant.body_extraction_misses"),
+        )
+        .returning(Some(7))
         .expect_increment_metric(Some(1), Some(1))
         .expect_get_buffer_bytes(Some(BufferType::PluginConfiguration))
         .returning(Some(cfg.as_bytes()))
